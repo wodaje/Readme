7d1c4c7e71d150000701e633a34d75ca2713e194
Jeff Woda <wodahouse@gmail.com>
2021-01-11 21:13:21 -0500

    added text edit input option for description section

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
- # Bla
+ # TESTETSETSETWAETWQETWQET
 
 ## Description 
+There once was a rabbit that was looking for a home.
 
-Your GitHub profile is an extremely important aspect of your public identity as a developer. A well-crafted one allows you to show off your work to other developers as well as potential employers. An important component of your GitHub profile—and one that many new developers often overlook—is the README.md file.
+Where he went was totally unknown!
 
-The quality of a README often differentiates a good project from a bad project. A good one takes advantage of the opportunity to explain and showcase what your application does, justify the technologies used, and even talk about some of the challenges you faced and features you hope to implement in the future. A good README helps you stand out among the large crowd of developers putting their work on GitHub.
+Since he was never seen again!
 
-There's no one right way to structure a good README. There is one very wrong way, however, and that is to not include a README at all or to create a very anemic one. This guide outlines a few best practices. As you progress in your career, you will develop your own ideas about what makes a good README.
+After the Fox ate him for breakfast. What can I say this is what happened.
+Does this make a good kids story???? 
+I don't think so!
 
-At a minimum, your project README needs a title and a short description explaining the what, why, and how. What was your motivation? Why did you build this project? (Note: The answer is not "Because it was a homework assignment.") What problem does it solve? What did you learn? What makes your project stand out? If your project has a lot of features, consider adding a heading called "Features" and listing them here.
-
-If you're new to Markdown, read the GitHub guide on [Mastering Markdown](https://guides.github.com/features/mastering-markdown/).
-
-If you need an example of a good README, check out [the VSCode repository](https://github.com/microsoft/vscode).
-
+	Do you ??
 
 ## Table of Contents (Optional)
 
--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const questions = () =>{
         name: "pro_title",
         message: "Please input the title for your Project"
         },
-
+    {
+        type:"editor",
+        name: "description",
+        message: "Please add a description of your Project"
+        }
 
     ])
 } 
@@ -22,19 +26,7 @@ const genMD = (answers) =>{
 return ` # ${answers.pro_title}
 
 ## Description 
-
-Your GitHub profile is an extremely important aspect of your public identity as a developer. A well-crafted one allows you to show off your work to other developers as well as potential employers. An important component of your GitHub profile—and one that many new developers often overlook—is the README.md file.
-
-The quality of a README often differentiates a good project from a bad project. A good one takes advantage of the opportunity to explain and showcase what your application does, justify the technologies used, and even talk about some of the challenges you faced and features you hope to implement in the future. A good README helps you stand out among the large crowd of developers putting their work on GitHub.
-
-There's no one right way to structure a good README. There is one very wrong way, however, and that is to not include a README at all or to create a very anemic one. This guide outlines a few best practices. As you progress in your career, you will develop your own ideas about what makes a good README.
-
-At a minimum, your project README needs a title and a short description explaining the what, why, and how. What was your motivation? Why did you build this project? (Note: The answer is not "Because it was a homework assignment.") What problem does it solve? What did you learn? What makes your project stand out? If your project has a lot of features, consider adding a heading called "Features" and listing them here.
-
-If you're new to Markdown, read the GitHub guide on [Mastering Markdown](https://guides.github.com/features/mastering-markdown/).
-
-If you need an example of a good README, check out [the VSCode repository](https://github.com/microsoft/vscode).
-
+${answers.description}
 
 ## Table of Contents (Optional)
 
@@ -99,7 +91,7 @@ Go the extra mile and write tests for your application. Then provide examples on
 const init = async () => {
     try {
         const  answers = await questions()
-        console.log(answers.pro_title)
+        console.log(answers.description)
         const MD = genMD(answers)
         await writeFsA("README.md", MD)
         console.log("Successfully wrote README.md")
