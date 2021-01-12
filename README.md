feac0ac0945a3f96fac65dd46f1aa5319948cd92
Jeff Woda <wodahouse@gmail.com>
2021-01-12 17:38:35 -0500

    updated functionality missing only badge

--- README.md
+++ README.md
@@ -1,72 +1,48 @@
- # TESTETSETSETWAETWQETWQET
+ # undefined
 
 ## Description 
-There once was a rabbit that was looking for a home.
+undefined
 
-Where he went was totally unknown!
+## Table of Contents
 
-Since he was never seen again!
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+* [License](#License)
 
-After the Fox ate him for breakfast. What can I say this is what happened.
-Does this make a good kids story???? 
-I don't think so!
-
-	Do you ??
-
-## Table of Contents (Optional)
-
-If your README is very long, add a table of contents to make it easy for users to find what they need.
-
-* [Installation](#installation)
-* [Usage](#usage)
-* [Credits](#credits)
-* [License](#license)
+<=======================================================================================>
 
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+undefined
 
 
 ## Usage 
 
-Provide instructions and examples for use. Include screenshots as needed. 
-
-
-## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
-
-
-## License
-
-The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/)
-
-
----
-
-🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-
-Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
+undefined
 
 ## Contributing
 
-If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+undefined
 
 ## Tests
 
-Go the extra mile and write tests for your application. Then provide examples on how to run them.
+undefined
 
+
+## Questions
+
+Github user name: [undefined](https://github.com/undefined)
+
+Contact email: [undefined]("mailto:undefined")
+
+## License
+
+Licensed under the [ ](  License.txt) license.
 
 ---
+
 © 2020 Wodahouse.com DWYW license (Wo What You Want)
--- index.js
+++ index.js
@@ -7,92 +7,125 @@ const writeFsA = util.promisify(fs.writeFile)
 const questions = () =>{
     
     return inquirer.prompt([
-    {
+   {
         type:"input",
         name: "pro_title",
-        message: "Please input the title for your Project"
+        message: "Please input the title for your <Project>"
         },
     {
         type:"editor",
         name: "description",
-        message: "Please add a description of your Project"
+        message: "Please add a description of your <Project>"
+        },
+    {
+        type:"editor",
+        name: "install",
+        message: `Please add instructions on installation = Heading is <Installation>`
+        },        
+    {
+        type:"editor",
+        name: "usage",
+        message: `Please add details on usage = Heading is <Usage>`
+        },
+    {
+        type:"editor",
+        name: "contr",
+        message: `Please add contribution guidlines = Heading is <Contributing>`
+        },        
+    {
+        type:"editor",
+        name: "test",
+        message: `Please add testing instructions = Heading is <Tests>`
+        }, 
+    {
+        type:"input",
+        name: "git",
+        message: "Please enter your GitHub username for the <Questions> Section"
+        },        
+    {
+        type:"input",
+        name: "email",
+        message: "Please enter your contact email for the <Questions> Section"
+        },
+    {
+        type:"list",
+        name: "lice",
+        message: "Please choose your desired License",
+        choices: liceX
         }
-
     ])
 } 
 
-const genMD = (answers) =>{
 
-return ` # ${answers.pro_title}
+const liceX = [
+    {name:'GNU GPLv3'},
+    {name:'MIT'},
+    {name:'Apache'},
+    {name:'ISC'}
+]
+
+
+//https://img.shields.io/apm/l/vim-mode
+
+
+
+const genMD = (out) =>{
+
+return ` # ${out.pro_title}
 
 ## Description 
-${answers.description}
+${out.description}
 
-## Table of Contents (Optional)
+## Table of Contents
 
-If your README is very long, add a table of contents to make it easy for users to find what they need.
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+* [License](#License)
 
-* [Installation](#installation)
-* [Usage](#usage)
-* [Credits](#credits)
-* [License](#license)
+<=======================================================================================>
 
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+${out.install}
 
 
 ## Usage 
 
-Provide instructions and examples for use. Include screenshots as needed. 
-
-
-## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
-
-
-## License
-
-The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/)
-
-
----
-
-🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-
-Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
+${out.usage}
 
 ## Contributing
 
-If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+${out.contr}
 
 ## Tests
 
-Go the extra mile and write tests for your application. Then provide examples on how to run them.
+${out.test}
 
+
+## Questions
+
+Github user name: [${out.git}](https://github.com/${out.git})
+
+Contact email: [${out.email}]("mailto:${out.email}")
+
+## License
+
+Licensed under the [${out.lice}](${out.lice} License.txt) license.
 
 ---
+
 © 2020 Wodahouse.com DWYW license (Wo What You Want)`
 
 }
 
 const init = async () => {
     try {
-        const  answers = await questions()
-        console.log(answers.description)
-        const MD = genMD(answers)
+        const  out = await questions()
+        const MD = genMD(out)
         await writeFsA("README.md", MD)
         console.log("Successfully wrote README.md")
     }catch(error){
